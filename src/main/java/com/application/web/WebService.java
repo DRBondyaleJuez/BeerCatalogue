3b2328dda581b87f2dfc84db236cf3006fdee1ca
Daniel RBJ <d.r.bond.juez@hotmail.com>
2023-03-09 14:22:43 +0100

    Added the endpoint methods to fullfill the test's requirements

--- src/main/java/com/application/web/WebService.java
+++ src/main/java/com/application/web/WebService.java
@@ -1,10 +1,7 @@
 package com.application.web;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * This class acts as a rest controller, this means it is the class defining and
@@ -19,6 +16,46 @@ public class WebService {
 
     @GetMapping("/index")
     public String getGreetings() {
+        return "Hello world";
+    }
+
+    @GetMapping("/allbeers")
+    public String getBeerList() {
+        return "Hello world";
+    }
+
+    @GetMapping("/beers")
+    public String getBeerDetail() {
+        return "Hello world";
+    }
+
+    @PostMapping("/beers")
+    public String addNewBeer() {
+        return "Hello world";
+    }
+
+    @PutMapping("/beers")
+    public String updateBeerInfo() {
+        return "Hello world";
+    }
+
+    @GetMapping("/allmanufacturers")
+    public String getManufacturerList() {
+        return "Hello world";
+    }
+
+    @GetMapping("/manufacturers")
+    public String getManufacturerDetail() {
+        return "Hello world";
+    }
+
+    @PostMapping("/manufacturers")
+    public String addNewManufacturer() {
+        return "Hello world";
+    }
+
+    @PutMapping("/manufacturers")
+    public String updateManufacturerInfo() {
         return "Hello world";
     }
 
