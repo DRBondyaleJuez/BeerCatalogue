a65de76e4fad98ac67cab1a228334ece4e50543a
Daniel RBJ <d.r.bond.juez@hotmail.com>
2023-03-09 16:26:23 +0100

    Defined the implementations of the methods associated with each EndPoint and defined the methods in the controller that will act as an intermediary between the persistence and the webService.

--- src/main/java/com/application/controller/Controller.java
+++ src/main/java/com/application/controller/Controller.java
@@ -1,4 +1,40 @@
 package com.application.controller;
 
+import com.application.model.Beer;
+import com.application.model.Manufacturer;
+
+import java.util.ArrayList;
+
 public class Controller {
+    public ArrayList<String> getBeerList() {
+        return null;
+    }
+
+    public Beer findBeer(String beerName) {
+        return null;
+    }
+
+    public boolean addNewBeer(Beer newBeer) {
+        return false;
+    }
+
+    public boolean updateBeer(Beer beerToUpdate) {
+        return false;
+    }
+
+    public ArrayList<String> getManufacturerList() {
+        return null;
+    }
+
+    public Manufacturer findManufacturer(String manufacturerName) {
+        return null;
+    }
+
+    public boolean addNewManufacturer(Manufacturer newManufacturer) {
+        return false;
+    }
+
+    public boolean updateManufacturer(Manufacturer manufacturerToUpdate) {
+        return false;
+    }
 }
--- src/main/java/com/application/web/WebService.java
+++ src/main/java/com/application/web/WebService.java
@@ -1,7 +1,14 @@
 package com.application.web;
 
 
+import com.application.controller.Controller;
+import com.application.model.Beer;
+import com.application.model.Manufacturer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 /**
  * This class acts as a rest controller, this means it is the class defining and
@@ -10,7 +17,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class WebService {
 
+    private final Controller controller;
+
     public WebService() {
+        controller = new Controller();
     }
 
 
@@ -19,44 +29,99 @@ public class WebService {
         return "Hello world";
     }
 
+    //------------Beer Related EndPoints
+
     @GetMapping("/allbeers")
-    public String getBeerList() {
-        return "Hello world";
+    public ResponseEntity<ArrayList<String>> getBeerList() {
+
+        ArrayList<String> beerList = controller.getBeerList();
+
+        if(beerList.isEmpty()){
+            return new ResponseEntity<>(beerList, HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(beerList, HttpStatus.OK);
+        }
     }
 
     @GetMapping("/beers")
-    public String getBeerDetail() {
-        return "Hello world";
+    public ResponseEntity<Beer> getBeerDetails(@RequestParam String beerName) {
+        Beer beerSearched = controller.findBeer(beerName);
+
+        if(beerSearched == null){
+            return new ResponseEntity<>(beerSearched, HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(beerSearched, HttpStatus.OK);
+        }
     }
 
     @PostMapping("/beers")
-    public String addNewBeer() {
-        return "Hello world";
+    public ResponseEntity<String> addNewBeer(@RequestBody Beer newBeer) {
+        boolean beerAddedCorrectly = controller.addNewBeer(newBeer);
+
+        if(!beerAddedCorrectly){
+            return new ResponseEntity<>("Unable to add new beer. Probably it already exists in the database", HttpStatus.NOT_ACCEPTABLE);
+        } else {
+            return new ResponseEntity<>("Beer (" + newBeer.getName() + ") has been added correctly to the database.", HttpStatus.OK);
+        }
     }
 
     @PutMapping("/beers")
-    public String updateBeerInfo() {
-        return "Hello world";
+    public ResponseEntity<String> updateBeerInfo(@RequestBody Beer beerToUpdate) {
+        boolean beerUpdatedCorrectly = controller.updateBeer(beerToUpdate);
+
+        if(!beerUpdatedCorrectly){
+            return new ResponseEntity<>("Unable to update beer. Probably it doesn't exists in the database", HttpStatus.NOT_ACCEPTABLE);
+        } else {
+            return new ResponseEntity<>("Beer (" + beerToUpdate.getName() + ") has been updated correctly in the database.", HttpStatus.OK);
+        }
     }
 
+
+    //------------Manufacturer Related EndPoints
+
     @GetMapping("/allmanufacturers")
-    public String getManufacturerList() {
-        return "Hello world";
+    public ResponseEntity<ArrayList<String>>  getManufacturerList() {
+
+        ArrayList<String> manufacturerList = controller.getManufacturerList();
+
+        if(manufacturerList.isEmpty()){
+            return new ResponseEntity<>(manufacturerList, HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(manufacturerList, HttpStatus.OK);
+        }
     }
 
     @GetMapping("/manufacturers")
-    public String getManufacturerDetail() {
-        return "Hello world";
+    public ResponseEntity<Manufacturer>  getManufacturerDetail(@RequestParam String manufacturerName) {
+        Manufacturer manufacturerSearched = controller.findManufacturer(manufacturerName);
+
+        if(manufacturerSearched == null){
+            return new ResponseEntity<>(manufacturerSearched, HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(manufacturerSearched, HttpStatus.OK);
+        }
     }
 
     @PostMapping("/manufacturers")
-    public String addNewManufacturer() {
-        return "Hello world";
+    public ResponseEntity<String> addNewManufacturer(@RequestBody Manufacturer newManufacturer) {
+        boolean manufacturerAddedCorrectly = controller.addNewManufacturer(newManufacturer);
+
+        if(!manufacturerAddedCorrectly){
+            return new ResponseEntity<>("Unable to add new beer. Probably it already exists in the database", HttpStatus.NOT_ACCEPTABLE);
+        } else {
+            return new ResponseEntity<>("Beer (" + newManufacturer.getName() + ") has been added correctly to the database.", HttpStatus.OK);
+        }
     }
 
     @PutMapping("/manufacturers")
-    public String updateManufacturerInfo() {
-        return "Hello world";
+    public ResponseEntity<String> updateManufacturerInfo(@RequestBody Manufacturer manufacturerToUpdate) {
+        boolean beerUpdatedCorrectly = controller.updateManufacturer(manufacturerToUpdate);
+
+        if(!beerUpdatedCorrectly){
+            return new ResponseEntity<>("Unable to update manufacturer. Probably it doesn't exists in the database", HttpStatus.NOT_ACCEPTABLE);
+        } else {
+            return new ResponseEntity<>("Manufacturer (" + manufacturerToUpdate.getName() + ") has been updated correctly in the database.", HttpStatus.OK);
+        }
     }
 
 
